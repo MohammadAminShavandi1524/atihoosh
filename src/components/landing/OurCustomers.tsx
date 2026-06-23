975b64243544118f51457f2b029a810b8749d943
MohammadAminShavandi1524 <maminshavandi1524@gmail.com>
2026-06-23 00:07:24 -0700

    our customers changed in landing

--- src/components/landing/OurCustomers.tsx
+++ src/components/landing/OurCustomers.tsx
@@ -11,48 +11,12 @@ const OurCustomers = ({}: OurCustomersProps) => {
   const t = useTranslations("HomePage.OurCustomersSection");
 
   return (
-    <div className="mt-20 border-t border-t-border pt-15">
+    <div className="border-t-border mt-20 border-t pt-15">
       <div className="w90 flex flex-col items-center 2xl:px-30">
-        <div className="text-primary mb-6 text-center text-[22px]">
-          {t("badge")}
-        </div>
-
-        <div className="mb-8 text-center text-[48px]">{t("title")}</div>
-
-        <div className="mb-12 text-center text-lg text-[#757575]">
-          {t("description")}
-        </div>
-
-        {/* brands */}
-        <div className="mb-22 flex items-center gap-x-20">
-          <Image
-            className="rounded-full"
-            src="/atiabzar.png"
-            alt="ati-abzar"
-            width={100}
-            height={100}
-          />
-          <Image
-            className="rounded-full"
-            src="/atiabzar.png"
-            alt="ati-abzar"
-            width={100}
-            height={100}
-          />
-          <Image
-            className="rounded-full"
-            src="/atiabzar.png"
-            alt="ati-abzar"
-            width={100}
-            height={100}
-          />
-        </div>
+        <div className="text-[40px] mb-16 mt-4 font-medium">{t("badge")}</div>
 
         {/* companies */}
-        <div className="grid grid-cols-3 gap-px bg-border">
-          <Companies />
-          <Companies />
-          <Companies />
+        <div className="bg-border mt-10 grid grid-cols-3">
           <Companies />
           <Companies />
           <Companies />
