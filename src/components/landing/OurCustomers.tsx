1bfdcbf33918c4b26a64159aabc4088cde4bbe78
atihooshbonyanco <atihooshbonyanco@gmail.com>
2026-07-21 06:27:13 -0700

    tooltip and services added

--- src/components/landing/OurCustomers.tsx
+++ src/components/landing/OurCustomers.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { useLocale, useTranslations } from "next-intl";
 import { motion } from "framer-motion";
 import Companies from "./Companies";
+import Link from "next/link";
+import CustomTooltip from "../CustomTooltip";
 
 interface OurCustomersProps {}
 
@@ -18,42 +20,56 @@ const OurCustomers = ({}: OurCustomersProps) => {
 
         {/* companies */}
         <div className="bg-border mt-10 grid grid-cols-2">
-          <Companies />
-
-          {/*  */}
-          <motion.div
-            initial={false}
-            whileHover="hover"
-            className="bg-background text-muted flex cursor-pointer flex-col items-center gap-y-6 px-30 py-4"
+          <CustomTooltip
+            content={
+              locale === "en" ? "Ati Abzar Pishro website" : "  وب‌سایت آتی ابزار پیشرو"
+            }
           >
-            <motion.div
-              variants={{
-                hover: { scale: 1.1 },
-              }}
-              transition={{
-                duration: 0.35,
-                ease: [0.22, 1, 0.36, 1],
-              }}
-              className="flex h-[105px] flex-col justify-center"
-            >
-              <Image
-                className="rounded-full"
-                src="/atisanat.png"
-                alt="ati-sanat"
-                width={180}
-                height={80}
-              />
-            </motion.div>
-
-            <span className="text-muted-foreground text-lg font-medium">
-              {locale === "en"
-                ? "Ati Sanat Gostar Pouya"
-                : "آتی صنعت گستر پویا"}
-            </span>
-          </motion.div>
+            <Companies />
+          </CustomTooltip>
 
           {/*  */}
-          {/* <Companies /> */}
+
+          <CustomTooltip
+            content={
+              locale === "en"
+                ? "Ati Sanat Gostar Pouya website"
+                : "وب‌سایت آتی صنعت گستر پویا"
+            }
+          >
+            <Link href="/your-page" target="_blank">
+              <motion.div
+                initial={false}
+                whileHover="hover"
+                className="bg-background text-muted flex cursor-pointer flex-col items-center gap-y-6 px-30 py-4"
+              >
+                <motion.div
+                  variants={{
+                    hover: { scale: 1.1 },
+                  }}
+                  transition={{
+                    duration: 0.35,
+                    ease: [0.22, 1, 0.36, 1],
+                  }}
+                  className="flex h-[105px] flex-col justify-center"
+                >
+                  <Image
+                    className="rounded-full"
+                    src="/atisanat.png"
+                    alt="ati-sanat"
+                    width={180}
+                    height={80}
+                  />
+                </motion.div>
+
+                <span className="text-muted-foreground text-lg font-medium">
+                  {locale === "en"
+                    ? "Ati Sanat Gostar Pouya"
+                    : "آتی صنعت گستر پویا"}
+                </span>
+              </motion.div>
+            </Link>
+          </CustomTooltip>
         </div>
       </div>
     </div>
